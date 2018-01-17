34cf86b99bfb797f2ec3bac5fa99c7256c3f831d
csh <webfansplz@kobe.com>
2018-01-17 18:57:27 +0800

    canvas-bg

--- app/App.vue
+++ app/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <canvas id="canvas"></canvas>
     <transition>
       <router-view></router-view>
     </transition>
@@ -7,14 +8,33 @@
 </template>
 <script>
 import fetch from './utils/fetch';
+
 export default {
   name: 'app',
   data() {
     return {};
   },
-  created() {}
+  created() {},
+  mounted() {
+    require('./utils/particle');
+  }
 };
 </script>
 <style lang="less">
-
+* {
+  margin: 0;
+  padding: 0;
+}
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
+#app {
+  height: 100%;
+}
+canvas {
+  width: 100%;
+  height: 100%;
+}
 </style>
