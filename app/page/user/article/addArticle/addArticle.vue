47b49c048208cc1459bd2fc0426ffa40d93f0738
csh <308241863@qq.com>
2018-01-25 19:08:22 +0800

    category

--- app/page/user/article/addArticle/addArticle.vue
+++ app/page/user/article/addArticle/addArticle.vue
@@ -4,8 +4,11 @@
         <FormItem label="文章标题">
             <Input v-model="formItem.input" placeholder="Enter title..."></Input>
         </FormItem>
-        <FormItem label="文章标签">
-            <Input v-model="formItem.input" placeholder="Enter title..."></Input>
+        <FormItem label="文章标签" class="btn-box">
+            <i-button v-for="(item,i) in tagList" :key="i"  :class="{'current':tags.indexOf(item._id)>-1}" @click="chooseTaglist(item._id)">{{item.name}}</i-button>
+        </FormItem>
+        <FormItem label="文章分类"  class="btn-box">
+            <i-button v-for="(item,i) in cateGoryList" :key="i" :class="{'current':category.indexOf(item._id)>-1}" @click="chooseArticlelist(item._id)">{{item.name}}</i-button>   
         </FormItem>
         <FormItem label="文章内容">
             <mavonEditor @save-content="getContent"></mavonEditor>
@@ -14,6 +17,7 @@
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
 import mavonEditor from 'components/mavonEditor/mavonEditor';
 export default {
   data() {
@@ -29,17 +33,44 @@ export default {
         slider: [20, 50],
         textarea: ''
       },
-      content: ''
+      //文章内容
+      content: '',
+      //文章标签
+      tags: [],
+      //文章分类
+      category: []
     };
   },
   methods: {
     //获取内容
     getContent(val) {
       console.log(val);
+    },
+    //选择文章标签
+    chooseTaglist(id) {
+      let i = this.tags.indexOf(id);
+      i > -1 ? this.tags.splice(i, 1) : this.tags.push(id);
+    },
+    //选择文章分类
+    chooseArticlelist(id) {
+      let i = this.category.indexOf(id);
+      i > -1 ? this.category.splice(i, 1) : this.category.push(id);
     }
   },
   components: {
     mavonEditor
+  },
+  computed: {
+    ...mapState({
+      tagList: state => state.article.tagList,
+      cateGoryList: state => state.article.cateGoryList
+    })
+  },
+  created() {
+    //获取文章标签
+    this.$store.dispatch('article/getTags');
+    //获取文章分类
+    this.$store.dispatch('article/getCategory');
   }
 };
 </script>
